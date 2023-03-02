883dcf33a755a2185826ed91f182433bfbf999d1
manunamz <manunamz@pm.me>
2023-03-02 13:59:46 -0500

    plantit <0001fab4>

---
+++ lib/jekyll-semtree.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "jekyll-semtree/version"
+
+module Jekyll
+  module SemTree
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/jekyll-semtree/version.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Jekyll
+  module SemTree
+
+    VERSION = "0.0.1"
+
+  end
+end
